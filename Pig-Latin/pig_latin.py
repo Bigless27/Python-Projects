b2bd3eee8c3be88ea7520b6f0b8ad14f18b39e04
Bigless27 <lessonryan@gmail.com>
2016-02-03 19:00:53 -0500

    Initial Soulition to Pig Latin Sentence

--- Pig-Latin/pig_latin.py
+++ Pig-Latin/pig_latin.py
@@ -1,12 +1,14 @@
 class Pig_latin(object):
-  vowels = ["a", "e" , "i", "o", "u"]
+  vowels = ["a", "e" , "i", "o", "u", "A", "E", "I", "O", "U"]
 
 
   def __init__(self, sentence):
     self.sentence = sentence
 
+
   def convert_sentence(self):
     new_sentence = self.sentence.split(" ")
+    converted_sentence = []
     for word in new_sentence:
       counter = 0
       if word[0] in self.vowels:
@@ -17,8 +19,27 @@ class Pig_latin(object):
         if letter in self.vowels:
           counter += 1
         else:
-          pass
+          converted_sentence.append(''.join(self.word_converter(word)))
+          break
+    return (' ').join(converted_sentence)
+
+
+  def word_converter(self,word):
+    split_word = list(word)
+    solution = list(word)
+    for letter in split_word:
+      print letter
+      if letter not in self.vowels:
+        solution.remove(letter)
+        solution.append(letter)
+      else:
+        break
+    solution.append("ay")
+    return solution
+
+
+
 
 
 convert = Pig_latin("Hello there")
-convert.convert_sentence()
+print convert.convert_sentence()
